9eff73d04d6607455bc1aa8c94c1778dc5537205
Rohit Choudhary <91256649+Rohit1792@users.noreply.github.com>
2023-10-04 16:26:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function playMusic() {
     const body = document.body;
     const audio = document.getElementById("myaudio");
     
-    body.addEventListener("mouseover", () => {
+    body.addEventListener("mouseenter", () => {
         audio.play();
     });
 };
